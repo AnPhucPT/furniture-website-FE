1b03a537522d86b8d9394d57d37c920a45229927
Phucfpt <91855261+Phucfpt@users.noreply.github.com>
2023-08-13 15:42:13 +0700

    update cart and order controller

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -2,12 +2,19 @@ function cartController(app) {
     app.controller('CartController', function ($scope, $http, $rootScope, $timeout, CartService) {
         $scope.removeProduct = null;
         $scope.products = CartService.getCartFromLS();
+        $scope.OrderAddress = 'HCM city';
+
         $rootScope.removeProductItem = () => {
             if ($scope.removeProduct) {
                 $scope.products = $scope.products.filter((item) => item.id !== $scope.removeProduct.id);
             }
             CartService.setCartToLS($scope.products);
             $scope.hideModal();
+            $timeout(function () {
+                showSuccessToast(`Remove Product 
+                <span class="dark:text-red-300 text-red-500">${$scope.removeProduct.name}</span>
+                Successful`);
+            }, 700);
         };
 
         $scope.modal = $rootScope.initModal('#deleteModal');
@@ -25,35 +32,45 @@ function cartController(app) {
             $scope.removeProduct = product;
         };
 
-        $scope.submit = () => {
-            $rootScope.loading = true;
-            const orderItemDtos = $scope.products.map((product) => ({
-                quantity: product.quantity,
-                product,
-            }));
-            const address = 'HCM';
-            const formData = { orderItemDtos, address };
-            $http
-                .post('http://localhost:8080/api/order', formData, {
-                    headers: {
-                        Authorization: 'Bearer ' + localStorage.getItem('access_token') || '',
-                    },
-                })
-                .then((res) => {
-                    const data = res.data;
-                    if (data.success) {
-                        $scope.products = [];
-                        CartService.setCartToLS([]);
-                    }
-                })
-                .catch((err) => {
-                    Promise.reject(err);
-                })
-                .finally(() =>
-                    $timeout(function () {
-                        $rootScope.loading = false;
-                    }, 300),
-                );
+        $scope.submit = (form) => {
+            if (form.$valid) {
+                $rootScope.loading = true;
+                const orderItemDtos = $scope.products.map((product) => ({
+                    quantity: product.quantity,
+                    product
+                }));
+                const formData = { orderItemDtos, address: $scope.OrderAddress };
+                $http
+                    .post('http://localhost:8080/api/order', formData, {
+                        headers: {
+                            Authorization: 'Bearer ' + localStorage.getItem('access_token') || ''
+                        }
+                    })
+                    .then((res) => {
+                        const data = res.data;
+                        if (data.success) {
+                            $scope.products = [];
+                            CartService.setCartToLS([]);
+                        }
+                        if (res?.data?.success) {
+                            $scope.products = [];
+                            CartService.setCartToLS([]);
+                            $timeout(function () {
+                                showSuccessToast(`Order Successful`);
+                            }, 700);
+                        } else {
+                            alert(res?.data?.message || 'Server interval');
+                        }
+                    })
+                    .catch((err) => {
+                        Promise.reject(err);
+                    })
+                    .finally(() =>
+                        $timeout(function () {
+                            $rootScope.loading = false;
+                        }, 300)
+                    );
+            }
         };
 
         $scope.getTotalPrice = () => {
